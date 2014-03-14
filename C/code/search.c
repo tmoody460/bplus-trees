6b2bb9a2e3ac509814e1e7a1d9d03c8aa70d04ae
Tracy Moody <tmoody@cse.unl.edu>
2014-03-14 16:05:34 -0700

    no way search will work

--- C/code/search.c
+++ C/code/search.c
@@ -10,6 +10,15 @@ void search_user_tree(int query, user_node* current);
 void search_message_tree(int query, message_node* current);
 void search_time_tree(float query, time_node* current);
 
+int main(){
+	
+	location_node* node = (location_node*) malloc(sizeof(location_node));
+	
+	search_location_tree((char*)&"Nebraska", node);
+
+	printf("stuff %s\n", node->state);
+}
+
 void search_user_tree(int query, user_node* current) {
 	int i = 0;
 	int cmp_result = -1;
@@ -76,9 +85,45 @@ void search_location_tree(char* query, location_node* current) {
 	int cmp_result = -1;
 	
 	read_location_node(current, (char *)&LOCATION_ROOT_PATH);
-	int beginning, middle, end;
 
-	int query_found = FALSE;
+	int beginning, middle, end;
+	int cmp_result = 0;
+	int exact_match_exists = FALSE;
+	while(current->is_leaf == FALSE){
+		beginning = 0;
+		end = current->num_filled - 1;
+		while (beginning <= end){
+			middle = (beginning + end) / 2;
+			cmp_result = strcmp(current->keys[middle], query);
+			if(cmp_result == 0){
+				exact_match_exists = TRUE;
+			}
+			
+			if(middle - 1 < 0 && end == 0){
+				middle = 0;
+				found = TRUE;
+			}else if(start == current->num_filled){
+				printf("2 if\n");
+				middle = start;
+				found = TRUE;
+			}else if(cmp_result >= 0 
+				&& strcmp(current->keys[middle-1], query) < 0){
+				printf("3 if\n");
+				found = TRUE;
+			}else if (cmp_result < 0){
+				printf("4 if\n");
+				start = middle + 1;
+			}else if (cmp_result >= 0){
+				printf("5 if\n");
+				end = middle - 1;
+			}
+		}
+		read_node(current, current->children[middle]);
+	}
+	
+	
+	
+	/*int query_found = FALSE;
 	while(current->is_leaf == FALSE){
 		for(i = 0; i < current->num_filled; i++){
 			cmp_result = strcmp(current->keys[i], query);
@@ -94,12 +139,16 @@ void search_location_tree(char* query, location_node* current) {
 			read_location_node(current, current->children[i]);
 		}
 	}
+	*/
+	
 	i=0;
-	cmp_result = strcmp(current->keys[i], query);
-	while(cmp_result != 0 && i < current->num_filled){
+	do{
+		cmp_result = strcmp(current->keys[i], query);
 		i++;
-	}
-	if(i == current->num_filled){
+	}while(cmp_result != 0 && i < current->num_filled);
+	i--;
+	
+	if(i == current->num_filled && exact_match_exists){
 		read_location_node(current, current->children[FAN_OUT-1]);
 	}
 }
